c36821f62d0ac7f807afd0be59de6d22005ad535
danghoangnam <88759047+danghoangnam@users.noreply.github.com>
2023-07-17 04:12:05 +0000

    Thêm test case

---
+++ user/user/test.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+class ClientAccessUser(TestCase):
+    def test_user_page_status(self):
+        response = self.client.get('/user/')
+        self.assertEqual(response.status_code, 201)
