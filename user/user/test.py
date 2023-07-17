7614ce0df5dfda0a64da73f23043c35f90111164
danghoangnam <88759047+danghoangnam@users.noreply.github.com>
2023-07-17 04:16:08 +0000

    sửa lại lỗi sai của test ClientAccessUser

--- user/user/test.py
+++ user/user/test.py
@@ -3,4 +3,4 @@ from django.test import TestCase
 class ClientAccessUser(TestCase):
     def test_user_page_status(self):
         response = self.client.get('/user/')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
