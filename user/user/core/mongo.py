dfa63d21b6c02b5e96d3775b4eefc99783d2696a
danghoangnam <88759047+danghoangnam@users.noreply.github.com>
2023-07-27 15:01:17 +0000

    thêm kết nối mongo

---
+++ user/user/core/mongo.py
@@ -0,0 +1,12 @@
+import pymongo
+import os
+
+HOST = os.environ.get('HOST')
+PORT = os.environ.get('PORT')
+
+USER = os.environ.get('USER')
+PASSWORD = os.environ.get('PASSWORD')
+
+clientMongo = pymongo.MongoClient(f'mongodb://{USER}:{PASSWORD}@{HOST}:{PORT}/')
+
+userTable = clientMongo['beoj']['user']
